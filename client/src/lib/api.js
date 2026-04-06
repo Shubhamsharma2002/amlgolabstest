0a0d141858586f33e311836b41523ea4979e30d6
Shubham Sharma <shubhamtech004@gmail.com>
2026-04-06 06:43:57 +0530

    change under client
    debuging

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = process.env.URL;
+
 
 export const apiRequest = async (endpoint, method, body) => {
   const token = localStorage.getItem("token");
